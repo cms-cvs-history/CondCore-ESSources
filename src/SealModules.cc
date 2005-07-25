ca685d83d734591867c8c9ea90e20ec825f701fb
cmsbuild <>
2005-07-25 12:57:53 +0000

    This commit was manufactured by cvs2git to create tag 'CondCore-ESSources- CondCore-0-0-2'.
    Sprout from master 2005-07-25 12:57:52 UTC Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch> 'reader runs'
    Delete:
        BuildFile
        doc/html/index.html
        doc/html/overview.html
        interface/AlignmentRetriever.h
        interface/PedestalRetriever.h
        src/AlignmentRetriever.cc
        src/SealModules.cc
        test/BuildFile
        test/trackerped.cpp

--- src/SealModules.cc
+++
@@ -1,7 +0,0 @@
-#include "PluginManager/ModuleDef.h"
-#include "FWCore/Framework/interface/SourceFactory.h"
-#include "CondCore/ESSources/interface/PedestalRetriever.h"
-#include "CondCore/ESSources/interface/AlignmentRetriever.h"
-DEFINE_SEAL_MODULE();
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(PedestalRetriever)
-DEFINE_ANOTHER_FWK_EVENTSETUP_SOURCE(AlignmentRetriever)
