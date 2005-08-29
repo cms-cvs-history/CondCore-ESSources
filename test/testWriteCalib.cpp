5ad3bed129735ef734e568afdfefed43e6414f39
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-08-29 11:46:55 +0000

    worked

--- test/testWriteCalib.cpp
+++ test/testWriteCalib.cpp
@@ -9,10 +9,15 @@
 #include <map>
 #include <iostream>
 int main(){
+  //std::string contact("oracle://devdb10/cms_xiezhen_dev");
+  std::string contact("sqlite_file://ecalcalib.db");
   pool::POOLContext::loadComponent( "SEAL/Services/MessageService" );
   pool::POOLContext::setMessageVerbosityLevel( seal::Msg::Error );
-  //cond::DBWriter w(std::string("sqlite_file:test.db")); 
-  cond::DBWriter w(std::string("sqlite_file:test.db")); 
+  const std::string userNameEnv = "POOL_AUTH_USER=cms_xiezhen_dev";
+  ::putenv( const_cast<char*>( userNameEnv.c_str() ) );
+  const std::string passwdEnv = "POOL_AUTH_PASSWORD=xiezhen123";
+  ::putenv( const_cast<char*>( passwdEnv.c_str() ) );
+  cond::DBWriter w(contact); 
   cond::IOV* iov=new cond::IOV;
   w.createContainer("EcalPedestals");
   EcalPedestals* ped1=new EcalPedestals;
@@ -25,7 +30,7 @@ int main(){
   item.m_variance=0.4;
   ped1->m_pedestals.insert(std::make_pair(channelId,item));
   std::cout<<2<<std::endl;
-
+  
   channelId=1858;
   item.m_mean=0.5;
   item.m_variance=0.94;
@@ -37,17 +42,15 @@ int main(){
   //assign IOV
   int tillrun=5;
   iov->iov.insert(std::make_pair(tillrun,tok));
-  std::string iovToken="akdkgh";
-    //w.write<cond::IOV>(iov);
   std::cout<<4<<std::endl;
-  /*
+  
   EcalPedestals* ped2=new EcalPedestals; //the user gives up the object ownership upon send it to the writer
   std::cout<<5<<std::endl;
   channelId=1878;
   item.m_mean=0.33;
   item.m_variance=0.44;
   ped2->m_pedestals.insert(std::make_pair(channelId,item));
-
+  
   channelId=1858;
   item.m_mean=0.56;
   item.m_variance=0.98;
@@ -58,12 +61,13 @@ int main(){
   tillrun=10;
   iov->iov.insert(std::make_pair(tillrun,tok2));
   std::cout<<6<<std::endl;
+  w.createContainer("IOV");
   std::string iovToken=w.write<cond::IOV>(iov);
-  */
+  
   w.commitTransaction();
   std::cout<<"about to commit2"<<std::endl;
   std::cout<<"writercommitted"<<std::endl;
   //register the iovToken to the metadata service
-  cond::MetaData metadata_svc("sqlite_file:test.db");
+  cond::MetaData metadata_svc(contact);
   metadata_svc.addMapping("EcalPedestals_2008_test", iovToken);  
 }
