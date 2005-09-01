e4aa5c521844a915fb305608d3250ef2214fd522
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2005-09-01 11:53:15 +0000

    added

---
+++ test/stubs/EcalPedestalsAnalyzer.cc
@@ -0,0 +1,56 @@
+
+/*----------------------------------------------------------------------
+
+Toy EDProducers and EDProducts for testing purposes only.
+
+----------------------------------------------------------------------*/
+
+#include <stdexcept>
+#include <string>
+#include <iostream>
+#include <map>
+#include "FWCore/Framework/interface/EDAnalyzer.h"
+#include "FWCore/Framework/interface/Event.h"
+#include "FWCore/Framework/interface/ESHandle.h"
+#include "FWCore/Framework/interface/MakerMacros.h"
+
+#include "FWCore/Framework/interface/EventSetup.h"
+#include "FWCore/ParameterSet/interface/ParameterSet.h"
+
+#include "CondFormats/EcalObjects/interface/EcalPedestals.h"
+#include "CondFormats/DataRecord/interface/EcalPedestalsRcd.h"
+
+using namespace std;
+
+namespace edmtest
+{
+  class EcalPedestalsAnalyzer : public edm::EDAnalyzer
+  {
+  public:
+    explicit  EcalPedestalsAnalyzer(edm::ParameterSet const& p) 
+    { }
+    explicit  EcalPedestalsAnalyzer(int i) 
+    { }
+    virtual ~ EcalPedestalsAnalyzer() { }
+    virtual void analyze(const edm::Event& e, const edm::EventSetup& c);
+  private:
+  };
+  
+  void
+   EcalPedestalsAnalyzer::analyze(const edm::Event& e, const edm::EventSetup& context)
+  {
+    using namespace edm::eventsetup;
+    // Context is not used.
+    std::cout <<" I AM IN RUN NUMBER "<<e.id().run() <<std::endl;
+    std::cout <<" ---EVENT NUMBER "<<e.id().run() <<std::endl;
+    edm::eventsetup::ESHandle<EcalPedestals> pPeds;
+    context.get<EcalPedestalsRcd>().get(pPeds);
+    //call tracker code
+    //
+    std::cout <<" Ecal peds for channel 1858 "<<std::endl;
+    int channelID=1858;
+    EcalPedestals* myped=const_cast<EcalPedestals*>(pPeds.product());
+    std::cout << "mean " << (myped->m_pedestals)[channelID].m_mean << ",variance" << (myped->m_pedestals)[channelID].m_variance << std::endl;    
+  }
+  DEFINE_FWK_MODULE(EcalPedestalsAnalyzer)
+}
