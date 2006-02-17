94e0f7005d4612f810a27e9fb12c494f975e4bfa
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2006-02-17 17:55:22 +0000

    fix

--- test/testWriteCalib.cpp
+++ test/testWriteCalib.cpp
@@ -26,17 +26,25 @@ int main(){
     cond::DBWriter iovwriter(*session1, "IOV");
     session1->startUpdateTransaction();
     cond::IOV* pediov=new cond::IOV;
-    Pedestals* myped=new Pedestals;
-    for(int ichannel=1; ichannel<=5; ++ichannel){
-      Pedestals::Item item;
+    for( int i=0; i<3; ++i){  //writing 4 peds
+      Pedestals* myped=new Pedestals;
+      for(int ichannel=1; ichannel<=5; ++ichannel){
+	Pedestals::Item item;
         item.m_mean=1.11*ichannel;
         item.m_variance=1.12*ichannel;
         myped->m_pedestals.push_back(item);
+      }
+      std::string mytok=pwriter.markWrite<Pedestals>(myped);
+      if(i<2){
+	pediov->iov.insert(std::make_pair(5,mytok)); 
+	//the first two ped valid until run 5
+      }else{
+	pediov->iov.insert(std::make_pair(10,mytok));
+	//the next two ped valid from run 6 until run 10
+      }
     }
-    std::string mytok=pwriter.markWrite<Pedestals>(myped);
-    pediov->iov.insert(std::make_pair(10,mytok));
     std::string pediovToken=iovwriter.markWrite<cond::IOV>(pediov);  
-    session1->commit();
+    session1->commit();//commit all in one
     session1->disconnect();
     delete session1;
     cond::MetaData metadata_svc("sqlite_file:test.db",*loader);
@@ -60,27 +68,33 @@ int main(){
     cond::DBWriter epedwriter(*session2,"EcalPedestals");
     cond::DBWriter eiovwriter(*session2, "IOV");
     cond::IOV* epediov=new cond::IOV;
-    EcalPedestals* eped=new EcalPedestals;
-    for(int ichannel=1658; ichannel<=1668; ++ichannel){
-      EcalPedestals::Item item;
-      item.mean_x1 =0.91+ichannel;
-      item.rms_x1  =0.17+ichannel;
-      item.mean_x6 =0.52+ichannel;
-      item.rms_x6  =0.03+ichannel;
-      item.mean_x12=0.16+ichannel;
-      item.rms_x12 =0.05+ichannel;
-      eped->m_pedestals.insert(std::make_pair(ichannel,item));
-    }
     session2->startUpdateTransaction();
-    std::string epedtok=epedwriter.markWrite<EcalPedestals>(eped);
-    //pool::Ref takes the ownership 
-    std::cout<<"ecalped1 token "<<epedtok<<std::endl;
-    //assign IOV
-    unsigned long tillrun=5;
-    epediov->iov.insert(std::make_pair(tillrun,epedtok));
+    for(int i=0; i<2; ++i){ //writing 2 ecalped
+      EcalPedestals* eped=new EcalPedestals;
+      for(int ichannel=1658; ichannel<=1668; ++ichannel){
+	EcalPedestals::Item item;
+	item.mean_x1 =(0.91*i)+ichannel;
+	item.rms_x1  =(0.17*i)+ichannel;
+	item.mean_x6 =(0.52*i)+ichannel;
+	item.rms_x6  =(0.03*i)+ichannel;
+	item.mean_x12=(0.16*i)+ichannel;
+	item.rms_x12 =(0.05*i)+ichannel;
+	eped->m_pedestals.insert(std::make_pair(ichannel,item));
+      }
+      std::string epedtok=epedwriter.markWrite<EcalPedestals>(eped);
+      //pool::Ref takes the ownership 
+      std::cout<<"ecalped token "<<epedtok<<std::endl;
+      //assign IOV
+      if(i<1){
+	epediov->iov.insert(std::make_pair(2,epedtok));
+      }else{
+	//valid forever
+	epediov->iov.insert(std::make_pair(edm::IOVSyncValue::endOfTime().eventID().run(),epedtok));
+      }
+    }
     std::string epediovtok=eiovwriter.markWrite<cond::IOV>(epediov); 
-    std::cout<<"ecalped iov token "<<epediovtok<<std::endl;  
-    session2->commit();
+    std::cout<<"ecalped iov token "<<epediovtok<<std::endl; 
+    session2->commit();//commit all in one
     session2->disconnect();
     delete session2;
     cond::MetaData ecalmetadata_svc(std::string("sqlite_file:ecalcalib.db"),*loader);
