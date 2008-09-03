364ecccef368fc903b8b9839d3908ebe8ee948ed
Zhen Xie <sha1-1aff87905f4e0572a16a433d78e0dff2e49ac9af@cern.ch>
2008-09-03 13:42:38 +0000

    fix stale pointer of datasvc; fix stop value for runnumber

--- src/PoolDBESSource.cc
+++ src/PoolDBESSource.cc
@@ -8,6 +8,7 @@
 //     <Notes on implementation>
 //
 // Author:      Zhen Xie
+// $Id$
 //
 // system include files
 #include "boost/shared_ptr.hpp"
@@ -92,7 +93,7 @@ fillRecordToTypeMap(std::multimap<std::string, std::string>& oToFill){
 //static cond::ConnectionHandler& conHandler=cond::ConnectionHandler::Instance();
 
 PoolDBESSource::PoolDBESSource( const edm::ParameterSet& iConfig ) :
-  m_session( new cond::DBSession ),initcall(true)
+  m_session( new cond::DBSession )
 {		
   //std::cout<<"PoolDBESSource::PoolDBESSource"<<std::endl;
   /*parameter set parsing and pool environment setting
@@ -239,11 +240,6 @@ PoolDBESSource::~PoolDBESSource()
 void 
 PoolDBESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey& iKey, const edm::IOVSyncValue& iTime, edm::ValidityInterval& oInterval ){
   //std::cout<<"PoolDBESSource::setIntervalFor"<<std::endl;
-  if (initcall) {
-    //std::cout<<"first call do nothing"<<std::endl;
-    initcall=false;
-    return;
-  }
   //LogDebug ("PoolDBESSource")<<iKey.name();
   std::string recordname=iKey.name();
   std::string objectname("");
@@ -284,6 +280,7 @@ PoolDBESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey& iKey
   if( !iovservice.isValid(leadingToken,abtime) ){
     os<<abtime;
     //throw cond::noDataForRequiredTimeException("PoolDBESSource::setIntervalFor",iKey.name(),os.str());
+    pooldb.commit();
     oInterval = edm::ValidityInterval::invalidInterval();
     return;
   }
@@ -294,7 +291,7 @@ PoolDBESSource::setIntervalFor( const edm::eventsetup::EventSetupRecordKey& iKey
     stop=edm::IOVSyncValue( edm::Timestamp(validity.second) );
   }else{
     start=edm::IOVSyncValue( edm::EventID(validity.first,0) );
-    stop=edm::IOVSyncValue( edm::EventID(validity.second,0) );
+    stop=edm::IOVSyncValue( edm::EventID(validity.second,edm::EventID::maxEventNumber()) );
   }
   oInterval = edm::ValidityInterval( start, stop );
   std::string payloadToken=iovservice.payloadToken(leadingToken,abtime);
